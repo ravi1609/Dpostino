b2d72ce486e995bdc1119c02b4bc2eb23d0ca956
ravi1609 <aims.ravi1609@gmail.com>
2017-10-23 23:23:32 +0530

    pushing the registration bean

--- src/main/java/com/dpostino/user/data/beans/UserRegistrationBean.java
+++ src/main/java/com/dpostino/user/data/beans/UserRegistrationBean.java
@@ -1,11 +1,61 @@
 package com.dpostino.user.data.beans;
 
 
-
+/**
+ * 
+ * @author JDEVS
+ * */
 public class UserRegistrationBean {
 
 	
+	//fields for the registration of the user
 	
+	private String first_name;
+	private String middle_name;
+	private String country_id;
+	private String last_name;
+	private String mobile_no;
+	private String email;
+	
+	//getters and setters
+	
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getMiddle_name() {
+		return middle_name;
+	}
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+	public String getCountry_id() {
+		return country_id;
+	}
+	public void setCountry_id(String country_id) {
+		this.country_id = country_id;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getMobile_no() {
+		return mobile_no;
+	}
+	public void setMobile_no(String mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 	
